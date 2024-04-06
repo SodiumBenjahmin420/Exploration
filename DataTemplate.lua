c7d75394f7a39c6a31d69acbf6d33103d82e968e
SodiumBenjahmin420 <161543165+SodiumBenjahmin420@users.noreply.github.com>
2024-04-06 17:41:11 -0500

    Update DataTemplate.lua

--- DataTemplate.lua
+++ DataTemplate.lua
@@ -20,7 +20,17 @@
 
 	Inventory = {};
 
-	SlotData = {};
+	SlotData = {
+	["1"] = nil;
+	["2"] = nil;
+	["3"] = nil;
+	["4"] = nil;
+	["5"] = nil;
+	["6"] = nil;
+	["8"] = nil;
+	["9"] = nil;
+	["0"] = nil;
+	};
 
 	
 }
